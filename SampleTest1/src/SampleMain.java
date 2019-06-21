5cf33b781cd5d727cb1a92a12c492de634b176df
suneel-s <marallasunilsainath@gmail.com>
2019-06-21 20:30:09 +0530

    Added code in SampleMain.java

--- SampleTest1/src/SampleMain.java
+++ SampleTest1/src/SampleMain.java
@@ -4,6 +4,7 @@ public class SampleMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside SampleMain");
+		System.out.println("main()");
 	}
 
 }
