1ce53ea32d7d008c8791dd3d386af3d3d81e5810
suneel-s <marallasunilsainath@gmail.com>
2019-06-21 20:59:32 +0530

    Added method1 in SampleMain.java

--- SampleTest1/src/SampleMain.java
+++ SampleTest1/src/SampleMain.java
@@ -1,10 +1,17 @@
 
 public class SampleMain {
 
+	public void method1()
+	{
+	System.out.println("Inside method1");	
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside SampleMain");
 		System.out.println("main()");
+		SampleMain sm=new SampleMain();
+		sm.method1();
+		
 	}
 
 }
